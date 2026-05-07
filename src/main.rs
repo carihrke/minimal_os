28fdf9fcae63ba94e0f28eecb2cd4ad1f35110c4
carihrke <carihrke@gmail.com>
2026-05-07 13:18:37 -0400

    added print!, println! macros

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ use core::panic::PanicInfo;
 
 mod vga_buffer;
 
-
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("Hello World{}", "!");
+    print!("secondline");
 
     loop {}
 }
 
-
 /// This function is called on panic.
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
